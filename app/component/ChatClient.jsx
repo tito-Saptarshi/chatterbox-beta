13e12e47f05731d238135515b8701d32b82156ae
tito-Saptarshi <saptarshi.tito.20@gmail.com>
2025-06-23 21:40:03 +0530

    general -u 23062025

--- app/component/ChatClient.jsx
+++ app/component/ChatClient.jsx
@@ -1,39 +1,46 @@
 "use client"
 
-import {
-  Chat,
-  Channel,
-  ChannelList,
-  Window,
-  ChannelHeader,
-  MessageList,
-  MessageInput,
-  Thread,
-  useCreateChatClient,
-} from "stream-chat-react";
-import "stream-chat-react/dist/css/v2/index.css";
+import { useState, useEffect } from 'react';
+import { useCreateChatClient, Chat, Channel, ChannelHeader, MessageInput, MessageList, Thread, Window } from 'stream-chat-react';
 
-const apiKey = "your-api-key";
-const userId = "user_2yk0qnV6gCpk4r0IDVQi8qe2Jgd";
-const token = "authentication-token";
+import 'stream-chat-react/dist/css/v2/index.css';
 
-const filters = { members: { $in: [userId] }, type: "messaging" };
-const options = { presence: true, state: true };
-const sort = { last_message_at: -1 };
+const apiKey = 'dz5f4d5kzrue';
+const userId = 'long-band-3';
+const userName = 'long';
+const userToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibG9uZy1iYW5kLTMiLCJleHAiOjE3NTA3MTIwOTB9.6oD9BSlKp12eclOTxSYP8rC_IChftwbepWQ32zJUcKI';
 
-export function ChatClient() {
+const user = {
+  id: userId,
+  name: userName,
+  image: `https://getstream.io/random_png/?name=${userName}`,
+};
+
+export function ChatClient ()  {
+  const [channel, setChannel] = useState();
   const client = useCreateChatClient({
     apiKey,
-    tokenOrProvider: token,
-    userData: { id: userId },
+    tokenOrProvider: userToken,
+    userData: user,
   });
 
-  if (!client) return <div>Loading...</div>;
+  useEffect(() => {
+    if (!client) return;
+
+    const channel = client.channel('messaging', 'custom_channel_id', {
+      image: 'https://getstream.io/random_png/?name=react',
+      name: 'Talk about React',
+      members: [userId],
+    });
+
+    setChannel(channel);
+  }, [client]);
+
+  if (!client) return <div>Setting up client & connection...</div>;
 
   return (
     <Chat client={client}>
-      <ChannelList sort={sort} filters={filters} options={options} />
-      <Channel>
+      <Channel channel={channel}>
         <Window>
           <ChannelHeader />
           <MessageList />
